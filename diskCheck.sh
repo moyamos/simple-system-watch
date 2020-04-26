4700c26c54a450b576d3049ac57dc371b3118983
root <root@tb28.trainbit.com>
2020-04-26 14:13:06 +0430

    sudos are removed.

--- diskCheck.sh
+++ diskCheck.sh
@@ -8,14 +8,14 @@ checkRaidHealth()
     if [ "$(cat /proc/interrupts | grep arcmsr | wc -c)" != "0" ]
     then
         [ "$DEBUG" == "1" ] && echo Areca
-	checkRes=$(sudo $arecaToolPath vsf info | tail -n +3 | head -n -2 | grep -cv 'Normal')
+	checkRes=$($arecaToolPath vsf info | tail -n +3 | head -n -2 | grep -cv 'Normal')
 	return $checkRes
     fi
 
     if [ "$(cat /proc/interrupts | grep megasas | wc -c)" != "0" ]
     then
         [ "$DEBUG" == "1" ] && echo MegaCLI
-	checkRes=$(sudo $megaSASToolPath -LDInfo -Lall -aALL | egrep -i 'State|Permission' | grep -vc Optimal)
+	checkRes=$($megaSASToolPath -LDInfo -Lall -aALL | egrep -i 'State|Permission' | grep -cv Optimal)
 	return $checkRes
     fi
 }
